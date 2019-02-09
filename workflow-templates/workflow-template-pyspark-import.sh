3d7abf9d85c3e802498052463e42b29bba3a9520
siddharth raghunath <siddharthraghunath794@gmail.com>
2019-02-09 19:32:40 +0400

    Update workflow-template-pyspark-import.sh

--- workflow-templates/workflow-template-pyspark-import.sh
+++ workflow-templates/workflow-template-pyspark-import.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
-bucket="gs://your-bucket"
+bucket="gs://sids-bucket"
+template_name="spark-mysql-import"
+cluster_name="spark-cluster"
+instance_name="streaming-practice-228618:us-central1:test-instance"
+table_name="customers"
+spark_file_path="spark-cloudsql/export_tables.py"
 
+# gsutil rm -r $bucket/$table_name/
 
-gcloud dataproc workflow-templates delete -q pyspark-import  && 
+# gcloud dataproc workflow-templates delete -q $template_name  && 
 
-gcloud beta dataproc workflow-templates create pyspark-import &&
+gcloud beta dataproc workflow-templates create $template_name &&
 
-gcloud beta dataproc workflow-templates set-managed-cluster pyspark-import --zone "us-east1-b" --cluster-name=spark-cloudsql \
- --scopes=default,sql-admin --initialization-actions=gs://dataproc-initialization-actions/cloud-sql-proxy/cloud-sql-proxy.sh \
- --properties=hive:hive.metastore.warehouse.dir=gs://staging.streaming-practice-228618.appspot.com/hive-warehouse \
+gcloud beta dataproc workflow-templates set-managed-cluster $template_name \
+ --zone "us-east1-b" --cluster-name=$cluster_name \
+ --scopes=default,sql-admin \
+ --initialization-actions=gs://dataproc-initialization-actions/cloud-sql-proxy/cloud-sql-proxy.sh \
+ --properties=hive:hive.metastore.warehouse.dir=$bucket/hive-warehouse \
  --metadata=enable-cloud-sql-hive-metastore=false \
- --metadata=additional-cloud-sql-instances=streaming-practice-228618:us-central1:test-instance=tcp:3307 \
- --master-machine-type n1-standard-1 --master-boot-disk-size 20 \
---num-workers 2 \
---worker-machine-type n1-standard-2 --worker-boot-disk-size 20 \
---image-version 1.3 &&
+ --metadata=additional-cloud-sql-instances=$instance_name=tcp:3307 \
+ --master-machine-type n1-standard-1 \
+ --master-boot-disk-size 20 \
+ --num-workers 2 \
+ --worker-machine-type n1-standard-2 \
+ --worker-boot-disk-size 20 \
+ --image-version 1.3 &&
 
-gcloud beta dataproc workflow-templates add-job pyspark $bucket/spark-cloudsql/export_all_tables.py --step-id=test123 --workflow-template=pyspark-import && 
+gcloud beta dataproc workflow-templates \
+add-job pyspark $bucket/$spark_file_path \
+--step-id=test123 \
+--workflow-template=$template_name && 
 
-gcloud beta dataproc workflow-templates instantiate pyspark-import
+gcloud beta dataproc workflow-templates instantiate $template_name
